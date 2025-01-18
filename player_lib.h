9ca9fa1436d00df6e68d20a760ed1c5aedbb3652
Dzobamain <knopkayt@gmail.com>
2025-01-18 15:34:23 +0100

    Added more precise movement with 360 degrees instead of 8 directions

--- player_lib.h
+++ player_lib.h
@@ -4,142 +4,147 @@
 #include "arena_and_player_info.h"
 #include <cmath>
 
-static char lastDirection = '\0'; // Для збереження останньої клавіші
-static int **savedPlayerDirection = nullptr; // Збережений напрямок руху
-static int pathLength = 0;
-static int currentIndex = 0;
+static char lastDirection = 0;
 
-void deleteSavedPath() {
-    if (savedPlayerDirection) {
-        for (int i = 0; i < pathLength; i++) {
-            delete[] savedPlayerDirection[i];
+bool isMovePossible(int x, int y)
+{
+    return (x >= 0 && y >= 0 && x < arenaLengthX && y < arenaHeightY && arena[y][x] != 1);
+}
+
+// Clear the saved path
+void clearPath(int **&playerPath, int &pathLength)
+{
+    if (playerPath)
+    {
+        for (int i = 0; i < pathLength; i++)
+        {
+            delete[] playerPath[i];
         }
-        delete[] savedPlayerDirection;
-        savedPlayerDirection = nullptr;
+        delete[] playerPath;
+        playerPath = nullptr;
+        pathLength = 0;
     }
 }
 
-int **calculatePath(char directionOfMovement)
+void movePlayerAlongPath(int &playerX, int &playerY, float angle, char directionOfMovement)
 {
-    float rad = to_radian(playerAngle);
-    double rayPositionY = playerPositionY + 0.5;
-    double rayPositionX = playerPositionX + 0.5;
-    double rayStepPositionY = sin(rad);
-    double rayStepPositionX = cos(rad);
-    pathLength = 0;
-
-    for (; pathLength < 100; pathLength++) /* Максимальна довжина шляху 100 */
-    {
-        rayPositionY += rayStepPositionY;
-        rayPositionX += rayStepPositionX;
-        int checkY = static_cast<int>(rayPositionY);
-        int checkX = static_cast<int>(rayPositionX);
-
-        if (checkX < 0 || checkY < 0 || checkX >= arenaLengthX || checkY >= arenaHeightY || arena[checkY][checkX] == 1)
-        {
-            break;
-        }
-    }
-
-    int **path = new int *[pathLength];
-    rayPositionY = playerPositionY + 0.5;
-    rayPositionX = playerPositionX + 0.5;
-    for (int i = 0; i < pathLength; i++)
-    {
-        path[i] = new int[2];
-        path[i][0] = static_cast<int>(rayPositionY);
-        path[i][1] = static_cast<int>(rayPositionX);
-        rayPositionY += rayStepPositionY;
-        rayPositionX += rayStepPositionX;
-    }
-
-    currentIndex = 0;
-    return path;
-}
-
-bool tryMovePlayer(int &playerX, int &playerY, float angle, char directionOfMovement)
-{
-    if (lastDirection == directionOfMovement && savedPlayerDirection)
-    {
-        /* Рух по збереженому шляху */
-        if (currentIndex >= pathLength || arena[savedPlayerDirection[currentIndex][0]][savedPlayerDirection[currentIndex][1]] == 1)
-        {
-            deleteSavedPath(); // Очищаємо збережений шлях
-            return false;
-        }
-
-        playerX = savedPlayerDirection[currentIndex][1];
-        playerY = savedPlayerDirection[currentIndex][0];
-        currentIndex++;
-        return true;
-    }
-    else
-    {
-        /* Обчислення нового шляху */
-        lastDirection = directionOfMovement;
-        deleteSavedPath();
-        savedPlayerDirection = calculatePath(directionOfMovement);
-    }
+    static int **playerPath = nullptr;
+    static int pathLength = 0;
 
     float rad = to_radian(angle);
-    int startX = playerX + playerSpeed * cos(rad);
-    int startY = playerY + playerSpeed * sin(rad);
 
-    if (startX < 0 || startY < 0 || startX >= arenaLengthX || startY >= arenaHeightY || arena[startY][startX] == 1)
+    if (directionOfMovement != lastDirection)
     {
-        deleteSavedPath();
-        return false;
+        clearPath(playerPath, pathLength);
+        lastDirection = directionOfMovement;
     }
 
-    playerX = startX;
-    playerY = startY;
-    return true;
+    // New direction
+    if (playerPath == nullptr)
+    {
+        int maxPathLength = arenaHeightY * arenaLengthX;
+        playerPath = new int *[maxPathLength];
+        int currentPathIndex = 0;
+
+        double currentY = playerY + 0.5;
+        double currentX = playerX + 0.5;
+
+        double stepY = sin(rad);
+        double stepX = cos(rad);
+
+        while (currentPathIndex < maxPathLength)
+        {
+            currentY += stepY;
+            currentX += stepX;
+
+            int gridY = static_cast<int>(currentY);
+            int gridX = static_cast<int>(currentX);
+
+            if (!isMovePossible(gridX, gridY))
+                break;
+
+            playerPath[currentPathIndex] = new int[2];
+            playerPath[currentPathIndex][0] = gridY;
+            playerPath[currentPathIndex][1] = gridX;
+            currentPathIndex++;
+        }
+
+        pathLength = currentPathIndex;
+
+        if (pathLength == 0)
+        {
+            delete[] playerPath;
+            playerPath = nullptr;
+        }
+    }
+
+    // Player movement along the path and moving the starting position
+    if (pathLength > 0)
+    {
+        playerX = playerPath[0][1];
+        playerY = playerPath[0][0];
+
+        int **newPath = new int *[pathLength - 1];
+        for (int i = 1; i < pathLength; i++)
+        {
+            newPath[i - 1] = playerPath[i];
+        }
+
+        delete[] playerPath[0];
+        delete[] playerPath;
+
+        playerPath = newPath;
+        pathLength--;
+    }
 }
 
-void tryRotatePlayer(float directionOfRotate)
+void tryRotatePlayer(float directionOfRotate, char directionOfMovement)
 {
-    playerAngle = static_cast<int>(playerAngle + directionOfRotate) % 360;
-    if (playerAngle < 0)
-    {
-        playerAngle += 360;
-    }
+    playerAngle = (static_cast<int>(playerAngle + directionOfRotate) % 360 + 360) % 360;
+    lastDirection = directionOfMovement;
 }
 
 void PlayerMovementFirstPerson(char directionOfMovement)
 {
-    int rotationSpeed = 10;
-    float moveAngle = playerAngle; /* За замовчуванням - вперед */
+    float speedOfRotate = 10;
+    float moveAngle = playerAngle;
     switch (directionOfMovement)
     {
     case 'w':
     case 'W':
-        tryMovePlayer(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        movePlayerAlongPath(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        // Forward
         break;
     case 's':
     case 'S':
         moveAngle += 180;
-        tryMovePlayer(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        movePlayerAlongPath(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        // Backward
         break;
     case 'a':
     case 'A':
         moveAngle -= 90;
-        tryMovePlayer(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        movePlayerAlongPath(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        // Left
         break;
     case 'd':
     case 'D':
         moveAngle += 90;
-        tryMovePlayer(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
-        break; 
+        movePlayerAlongPath(playerPositionX, playerPositionY, moveAngle, directionOfMovement);
+        // Right
+        break;
     case 'q':
     case 'Q':
-        tryRotatePlayer(-rotationSpeed);
+        tryRotatePlayer(-speedOfRotate, directionOfMovement);
+        // Rotate Left
         break;
     case 'e':
     case 'E':
-        tryRotatePlayer(rotationSpeed);
+        tryRotatePlayer(speedOfRotate, directionOfMovement);
+        // Rotate Right
         break;
     default:
-        return; /* Неправильне введення, без руху */
+        return;
     }
 }
 
